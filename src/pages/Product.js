06c7f6b76099c64f366889deeea69d56711d5ef1
Kaung Sithu <kgcthu.kev@gmail.com>
2022-02-04 15:28:41 +0630

    Category pages

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,5 +1,23 @@
+import { Col, Container, Row } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+import SingleItem from '../components/SingleItem'
+import { items } from '../data'
+
 const Product = () => {
-  return <div>Product</div>
+  const { productType } = useParams()
+  return (
+    <Container className='my-4'>
+      <Row xs={1} md={2} lg={5} className='g-4'>
+        {items
+          .filter((item) => item.type === productType)
+          .map((item) => (
+            <Col>
+              <SingleItem key={item.id} {...item} />
+            </Col>
+          ))}
+      </Row>
+    </Container>
+  )
 }
 
 export default Product
